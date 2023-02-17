fee40a9ba031f83dc26d00621039eaa440175b61
Ethan Evans <ethanalexevans@gmail.com>
2023-02-16 21:07:06 -0800

    fixed occasional corruption issue

--- src/heic2png/__main__.py
+++ src/heic2png/__main__.py
@@ -10,23 +10,22 @@ from tqdm import tqdm
 from PIL import Image
 import pillow_heif
 
-def convert_from_file(file: str, delete: bool = False):
+def convert_from_file(file: str, delete: bool = False, dest_dir: str = None):
     """Convert a single HEIC file to PNG"""
     file = os.path.abspath(file)
     heif_file = pillow_heif.read_heif(file)
-    image = Image.frombytes(
-        heif_file.mode,
-        heif_file.size,
-        heif_file.data,
-        "raw",
-    )
+    image = Image.frombytes(heif_file.mode, heif_file.size, heif_file.data, "raw", heif_file.mode, heif_file.stride)
     new_name = os.path.splitext(file)[0] + ".png"
-    image.save(new_name, format("png"))
+
+    if dest_dir:
+        new_name = os.path.join(dest_dir, os.path.basename(new_name))
+
+    image.save(new_name)
 
     if delete:
         os.remove(file)
 
-def convert_from_dir(dir: str, delete: bool = False):
+def convert_from_dir(dir: str, delete: bool = False, dest_dir: str = None):
     """Convert all HEIC files in a directory to PNG"""
     dir = os.path.abspath(dir)
 
@@ -38,7 +37,7 @@ def convert_from_dir(dir: str, delete: bool = False):
     progress = tqdm(total=len(targets), desc="Converting...", )
 
     for file in targets:
-        convert_from_file(os.path.join(dir, file), delete)
+        convert_from_file(os.path.join(dir, file), delete, dest_dir)
         progress.update(1)
 
 if __name__ == "__main__":
@@ -49,16 +48,18 @@ if __name__ == "__main__":
     parser_convert = subparsers.add_parser("dir", help="Convert all HEIC to PNG in a target directory")
     parser_convert.add_argument("dir", help="Target directory")
     parser_convert.add_argument("--delete", action="store_true", help="Delete the original HEIC files")
+    parser_convert.add_argument("dest", help="Destination directory")
 
     parser_convert = subparsers.add_parser("file", help="Convert a single HEIC to PNG")
     parser_convert.add_argument("file", help="Target file")
     parser_convert.add_argument("--delete", action="store_true", help="Delete the original HEIC file")
+    parser_convert.add_argument("dest", help="Destination directory")
 
     args = parser.parse_args()
 
     if args.command == "dir":
-        convert_from_dir(args.dir, args.delete)
+        convert_from_dir(args.dir, args.delete, args.dest)
     elif args.command == "file":
-        convert_from_file(args.file, args.delete)
+        convert_from_file(args.file, args.delete, args.dest)
     else:
         parser.print_help()
